f217a5b3a4c650c84c17e5c13880dce2302b3a4b
Robert Davidson <davidsonrobert1996@gmail.com>
2019-10-06 22:48:26 -0500

    fixed timer to countdown from 20 correctly, fixed timer to accurately tell if a question is wrong or right and then display it in the proper field. Added css styling to the page, its not dynamic currently.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,6 +9,7 @@ var correct = 0;
 var wrong = 0;
 
 
+
     if (question1 == "true") {
         correct++;
         }else{
@@ -34,20 +35,60 @@ var wrong = 0;
     }else{
         wrong++;
     }
-    
+    document.getElementById("timer").innerHTML = " ";
     document.getElementById("submit").style.visibility = "visible";
     document.getElementById("final-correct").innerHTML = "You got " + correct + " correct.";
     document.getElementById("final-wrong").innerHTML = "You got " + wrong + " wrong.";
-}
-
-var timeLeft = 30;
-var elem = document.getElementById('timer');
-var timerId = setInterval(countdown, 1000);
-
-function countdown() {
-    if (timeLeft == -1) {
-        clearTimeout(timerId);
-        doSomething();
     
-    }
+    
 }
+
+var seconds_left = 20;
+var interval = setInterval(function() {
+    document.getElementById('timer').innerHTML = --seconds_left;
+
+    if (seconds_left == 0)
+    {
+        document.getElementById('timer').innerHTML = 'Results';
+        clearInterval(interval);
+
+    var question1 = document.quiz.question1.value;
+    var question2 = document.quiz.question2.value;
+    var question3 = document.quiz.question3.value;
+    var question4 = document.quiz.question4.value;
+    var question5 = document.quiz.question5.value;
+    var correct = 0;
+    var wrong = 0;
+    
+    if (question1 == "true") {
+        correct++;
+        }else{
+            wrong++;
+        }
+    if (question2 == "true") {
+        correct++;
+    }else{
+        wrong++;
+    }
+    if (question3 == "false") {
+        correct++;
+    }else{
+        wrong++;
+    }
+    if (question4 == "false") {
+        correct++;
+    }else{
+        wrong++;
+    }
+    if (question5 == "true") {
+        correct++;
+    }else{
+        wrong++;
+    }
+    document.getElementById("timer").innerHTML = "Times Up! ";
+    document.getElementById("submit").style.visibility = "visible";
+    document.getElementById("final-correct").innerHTML = "You got " + correct + " correct.";
+    document.getElementById("final-wrong").innerHTML = "You got " + wrong + " wrong.";
+        
+    }
+}, 1000);
